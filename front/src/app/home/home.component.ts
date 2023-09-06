3c33f86b72a626a93dc164610afb5ff9cbc23028
Araneth25 <allccacogarciaaraneth@gmail.com>
2023-09-06 18:19:33 +0200

    Añadiendo font y UTF

---
+++ front/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule
+
+  ],
+  exports: [
+  ]
+})
+export class HomeComponent {
+
+}
