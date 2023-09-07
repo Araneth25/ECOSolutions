046071d604a62552475c1c542d813e42a2648097
Araneth25 <allccacogarciaaraneth@gmail.com>
2023-09-07 12:05:55 +0200

    Reducimiento de tamaño de movil

--- front/src/app/ecosolut/ecosolut.component.ts
+++ front/src/app/ecosolut/ecosolut.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
 
 interface EcoNavToggle{
@@ -11,12 +11,26 @@ interface EcoNavToggle{
   templateUrl: './ecosolut.component.html',
   styleUrls: ['./ecosolut.component.css']
 })
-export class EcosolutComponent {
+export class EcosolutComponent implements OnInit {
 
   @Output() onToggleEcoNav : EventEmitter<EcoNavToggle> = new EventEmitter();
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
+  @HostListener('window: resize', ['$event'])
+  onResize(event: any){
+    this.screenWidth = window.innerWidth;
+
+    if(this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleEcoNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+
+  ngOnInit(): void {
+      this.screenWidth = window.innerWidth;
+  }
 
   toggleCollapse(): void{
     this.collapsed = !this.collapsed;
