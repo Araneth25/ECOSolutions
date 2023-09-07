85843cb364dc169b717f2a290c0329be8860e59c
Araneth25 <allccacogarciaaraneth@gmail.com>
2023-09-07 12:53:55 +0200

    Agregando animacion en close

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
@@ -22,6 +22,7 @@ import { SettingsComponent } from './settings/settings.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- front/src/app/ecosolut/ecosolut.component.ts
+++ front/src/app/ecosolut/ecosolut.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 interface EcoNavToggle{
   screenWidth: number;
@@ -9,7 +10,33 @@ interface EcoNavToggle{
 @Component({
   selector: 'app-ecosolut',
   templateUrl: './ecosolut.component.html',
-  styleUrls: ['./ecosolut.component.css']
+  styleUrls: ['./ecosolut.component.css'],
+  animations: [
+    trigger('fadeInOut',[
+      transition(':enter',[
+          style({opacity:0}),
+        animate('350ms',
+          style({opacity:1})
+        )
+      ]),
+      transition(':leave',[
+        style({opacity:1}),
+      animate('350ms',
+        style({opacity:0})
+        )
+      ])
+    ]),
+    trigger('rotate',[
+      transition(':enter',[
+        animate('1000ms',
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}),
+            style({transform: 'rotate(2turn)', offset: '1'})
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class EcosolutComponent implements OnInit {
 
